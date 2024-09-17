f16cbe96fe6c42bf2e6bfc75d4bfaeaa9e00b356
Vinayak S <vinayaksukhalal@gmail.com>
2024-09-17 10:32:20 +0530

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 var express = require("express");
 var app = express();
 var bodyparser = require("body-parser");
+var cors = require("cors");
 
+// Enable CORS for all routes
+app.use(cors());
+
+// Parse incoming request bodies
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
-var main = require("./main");
-app.set("view engine", "ejs");
-app.set("views", "./views");
 
+var main = require("./main");
+
+
+
+// Start server
 app.listen("3000", () => {
   console.log("Server Started");
 });
+
+// Use the main route
 app.use("/", main);
--- main.js
+++ main.js
@@ -3,19 +3,68 @@ var router = express.Router();
 var mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // MongoDB Connection to cloud database
+// mongoose.connect(
+//   "mongodb+srv://vinayaksukhalal:12344321@cluster0.mcgbpy6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+// ).then(()=>{
+//     console.log("DB Connected")
+// });
 mongoose.connect(
-  "mongodb+srv://vinayaksukhalal:12344321@cluster0.mcgbpy6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-).then(()=>{
-    console.log("DB Connected")
-});
-
+    "mongodb+srv://vinayaksukhalal:goKeral%40123@cluster0.vqk0m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  ).then(()=>{
+      console.log("DB Connected")
+  });
 //Schema of the User
 var userSchema = mongoose.Schema({
   name: String,
   email: String,
-  mobileNo: Number,
+  phoneNumber: Number,
   password: String,
+  terms:Boolean
 });
 
 // Registering schema to mongoose
 var UserModal = mongoose.model("user", userSchema);
+router.post("/signup", async (req, res) => {
+  try {
+    console.log(req.body)
+    
+    var user = new UserModal({
+      name: req.body.name,
+      email:  req.body.email,
+      phoneNumber: req.body.phoneNumber,
+      password: req.body.password,
+      terms:req.body.terms
+    });
+
+    // Save the user and wait for the operation to complete
+    await user.save();
+
+    // Redirect after the user is successfully saved
+    res.status(200).json("User Created Succesfully");
+  } catch (error) {
+    // Handle any errors that might occur during the process
+    console.error("Error creating user:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+router.post("/login", async (req, res) => {
+  console.log(req.body);
+  var user = await UserModal.findOne({ email: req.body.email });
+  console.log(user)
+  if (user) {
+      if (user.password == req.body.password) {
+        email = user.email;
+        // res.redirect(`/home?id=${user._id}&msg=`);
+        res.status(200).json("Login Succesfull")
+      } else {
+        // res.render("login", { status: "Password is Wrong" });
+        res.status(404).json("Password is Wrong")
+      }
+  } else {
+   
+    res.status(404).json("UserName is Wrong")
+  }
+});
+
+
+module.exports = router
